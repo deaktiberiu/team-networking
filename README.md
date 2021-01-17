d81dc1fa081e3f9747913b9cdf50f351f0489dae
deakTiberiu <deaktiberiu93@gmail.com>
2021-01-17 12:37:19 +0200

    Show persons dynamically

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Teams related networking (linkedin,github)
 - [x] create index.html file with standrads
 - [x] create table that represents final design and structure (2 pers as example)
 - [x] table headers ---> [firstName, lastName, links]
-- [ ] remove 2 pers and load them from js
+- [x] remove 2 pers and load them from js
 - [ ] load team.json and render them in page
--- functions.js
+++ functions.js
@@ -1,2 +1,24 @@
-console.log("test")
+function insertPersons(data) {
+    const tbody = document.querySelector('#list tbody'); 
+    tbody.innerHTML = getPersonsHtml(data);
+    }
+
+function getPersonsHtml (persons) {
+    const personsLi = persons.map(function(persons){
+        return `<tr>
+                <td>${persons.firstName}</td>
+                <td>${persons.lastName}</td>
+                <td><a target="_blank" href="deaktiberiu.${persons.gitHub}">Git</a></td> 
+            </tr>`; 
+    });
+
+    return personsLi;
+    
+    }
+
+fetch('team.json')
+    .then(res => res.json())
+    .then(data => {
+        insertPersons(data);
+    });
 
