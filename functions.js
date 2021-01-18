81d21cfebb059a44baba4ba264f434afebdfeb7d
deakTiberiu <deaktiberiu93@gmail.com>
2021-01-18 05:11:49 +0200

    added primitive search engine

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+
 function insertPersons(data) {
     const tbody = document.querySelector('#list tbody'); 
     tbody.innerHTML = getPersonsHtml(data);
@@ -11,13 +12,30 @@ function getPersonHtml (person) {
     return `<tr>
                 <td>${person.firstName}</td>
                 <td>${person.lastName}</td>
-                <td><a target="_blank" href="https://github.com/${person.gitHub}">Git</a></td> 
+                <td class="end-cell"><a target="_blank" href="https://github.com/${person.gitHub}">Git</a></td> 
             </tr>`;
     }
+
+let allPersons= [];
+let data = [] ;
 
 fetch('team.json')
     .then(res => res.json())
     .then(data => {
+        allPersons= data;
         insertPersons(data);
     });
+    
+function searchPersons(text) {
+   return allPersons.filter(person => {
+       return person.firstName == text;
+   }); 
+}
+
+const search= document.getElementById('search'); 
+search.addEventListener("input", e => {
+    var text = e.target.value; 
+    const filtrate = searchPersons(text);
+    insertPersons(filtrate);
+});
 
